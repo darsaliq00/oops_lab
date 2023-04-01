93ed775ddbfb4a10ec35b528184d5c7bb45c0b5f
MIR AFAN <84376469+affanmir19@users.noreply.github.com>
2023-04-01 14:04:01 +0530

    Add files via upload

---
+++ Basic Programs/static_cast.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int total_classes;
+    cout << "Enter the total classes  : ";
+    cin >> total_classes;
+
+    int given_classes;
+    cout << "Enter the classes you have given : ";
+    cin >> given_classes;
+
+    float total = static_cast<double>(given_classes) / (total_classes)*100;
+
+    cout << "Your attendance is : " << total << endl;
+}
